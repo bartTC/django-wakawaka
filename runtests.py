7f79f75f5d1719f073044b59e9ec16c6ffd61e0c
Martin Mahner <martin@mahner.org>
2016-11-25 20:16:40 +0100

    The first test.

--- runtests.py
+++ runtests.py
@@ -6,35 +6,12 @@ from django import setup
 from django.conf import settings
 from django.test.runner import DiscoverRunner
 
-SETTINGS = {
-    'DATABASES': {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'dev.db',
-        },
-    },
-    'INSTALLED_APPS': [
-        'wakawaka',
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-    ],
-    'MIDDLEWARE_CLASSES': (
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-    ),
-}
+from wakawaka.tests.test_project import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wakawaka.tests.test_project.settings")
 
 def runtests(*test_args):
-    # Setup settings
-    if not settings.configured:
-        settings.configure(**SETTINGS)
-
     setup()
-
     test_runner = DiscoverRunner(verbosity=1)
     failures = test_runner.run_tests(['wakawaka'])
     if failures:
