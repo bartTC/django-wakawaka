c04479459d675730325b42dfc5e3dac05c4e6b88
Martin Mahner <martin@mahner.org>
2019-01-21 12:38:57 +0100

    Last cleanup.

--- wakawaka/tests/test_project/urls.py
+++ wakawaka/tests/test_project/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
